1d451cb76036a1c7d1cf5a074ad27e2c865d10b4
Damastier <Sergei@mail.com>
2022-09-18 20:20:20 +0300

    Partly implemented method getAllPosts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef, Renderer2, OnInit } from '@angular/core';
-import { interval, Observable, Subject, Subscription, timer } from 'rxjs';
+import { interval, Observable, of, Subject, Subscription, timer } from 'rxjs';
 import { map, filter, switchMap, take, distinctUntilChanged, debounceTime, tap, catchError } from 'rxjs/operators'
+import { ApiService } from './services/api-service.service';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +11,43 @@ import { map, filter, switchMap, take, distinctUntilChanged, debounceTime, tap, 
 export class AppComponent implements OnInit {
   @ViewChild('arrow') arrow!: ElementRef;
   public title = 'RxJs';
+  public allPosts: any;
 
   constructor(
     private renderer: Renderer2,
+    private apiService: ApiService,
   ) {}
 
   ngOnInit(): void {
     this.startClock();
+    this.getAllPosts();
   }
 
 /**
  * timer() - каждую секунду генирирует значение
- * map() - преобразовывает в секунды
+ * map() - Применяет заданную функцию (time * 360/60) к каждому значению, испускаемому исходным Observable, и выдает результирующие значения как Observable.
+ * в данном случае, преобразовывает в секунды.
  */
-  public startClock() {
+  public startClock(): any {
     timer(0,1000).pipe( map(time => time * 360/60)).subscribe({
       next: position => {
         this.renderer.setStyle(this.arrow.nativeElement, 'transform', `rotateZ(${position}deg`)
+      },
+      error: () => {}
+    })
+  }
+
+  public getAllPosts(): any {
+    this.apiService.getAllPosts().pipe(
+      map(posts => posts.map()),
+      tap(posts => { console.log(`p`,posts)}),
+      filter(posts => posts.userId === 1) // Отфильтруйте элементы, испускаемые исходным Observable, испуская только те, которые удовлетворяют указанному значению.
+      // distinctUntilChanged() Возвращает результат Observable, который испускает все значения, переданные исходным наблюдаемым объектом, 
+      // если они отличаются по сравнению с последним значением, испускаемым наблюдаемым результатом
+    ).subscribe({
+      next: posts => {
+        console.log(posts)
+        this.allPosts = posts;
       },
       error: () => {}
     })
